dc425749d68c22f17c6328a85d863b6d26cb3e5d
Hema Sai Reddy <158816242+BunnyHemaSaiReddy@users.noreply.github.com>
2024-08-08 19:10:10 +0530

    Update databaseconnection.py

--- databaseconnection.py
+++ databaseconnection.py
@@ -1,5 +1,4 @@
 import streamlit as st
 from streamlit.connections import SQLConnection
-'''conn = st.connection("my_sql", type=SQLConnection)'''
-st.connection
-st.connection("my_sql")
+conn = st.connection("my_sql", type=SQLConnection)
+
